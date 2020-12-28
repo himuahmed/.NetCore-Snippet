322e93d502dc3039c088bd075e85b8e477b4780e
Himu <mohsi.ahamed@gmail.com>
2020-12-28 13:41:24 +0600

    Add project files.

---
+++ storeApp/Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using storeApp.Models;
+using storeApp.Repository;
+
+namespace storeApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ItemRepository _itemRepository = null;
+
+        public ItemController()
+        {
+            _itemRepository = new ItemRepository();
+        }
+        public ViewResult GetAllItems()
+        {
+            var data =  _itemRepository.GetAllItems();
+
+            return View(data);
+        }
+
+        public ViewResult GetItem(int id)
+        {
+            var data =  _itemRepository.GetItem(id);
+            return View(data);
+        }
+
+        public List<Item> SearchItem(string Name)
+        {
+            return _itemRepository.SearchItem(Name);
+        }
+    }
+}
---
+++ storeApp/Repository/ItemRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using storeApp.Models;
+
+namespace storeApp.Repository
+{
+    public class ItemRepository
+    {
+        private List<Item> Items()
+        {
+            return new List<Item>()
+            {
+                new Item() {Id = 1, Name = "Coke", Type = "Grocery"},
+                new Item() {Id = 2, Name = "Chips", Type = "Grocery"},
+                new Item() {Id = 3, Name = "Cake", Type = "Grocery"},
+                new Item() {Id = 4, Name = "Eggs", Type = "Food"}
+            };
+        }
+
+        public List<Item> GetAllItems()
+        {
+            return Items();
+        }
+
+        public Item GetItem(int Id)
+        {
+            return Items().FirstOrDefault(x => x.Id == Id);
+        }
+
+        public List<Item> SearchItem(string Name)
+        {
+            return Items().Where(x => x.Name == Name).ToList();
+        }
+    }
+}
